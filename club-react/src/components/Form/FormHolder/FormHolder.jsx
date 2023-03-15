66cc4759c50bfec0c3e8bc52b2da31fad0384df3
dbw7 <90856056+dbw7@users.noreply.github.com>
2023-03-15 18:36:24 -0400

    current version

--- club-react/src/components/Form/FormHolder/FormHolder.jsx
+++ club-react/src/components/Form/FormHolder/FormHolder.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./FormHolder.css"
 
 const FormHolder = () => {
     const [counter, setCounter] = useState(0);
+    const [multipier, setMultiplier] = useState(1);
+    
+    const multipierRef = useRef();
     
     const buttonHandler = () =>{
         setCounter(counter + 1);
     }
     
+    const multiplierHandler = () =>{
+        console.log(multipierRef.current.value);
+    }
+    
+    
     return(
         <div className="form-holder">
             <h1>This is a form</h1>
             <h2>Counter: {counter}</h2>
-            <input></input>
-            <button onClick={buttonHandler}>Submit</button>
+            <h3>Multipier: {multipier}</h3>
+            <input type="number" ref={multipierRef}></input>
+            <button onClick={multiplierHandler}>Submit</button>
+            <button onClick={buttonHandler}>Increment</button>
         </div>
     )
     
